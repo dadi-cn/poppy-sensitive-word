84edfe0e4528b45dc0f3545eae1f4096cca61031
多厘 <zhaody901@126.com>
2022-03-24 16:40:31 +0800

    feat: 4.0 add mgr-app

---
+++ src/Http/Form/FormSensWordEstablish.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Poppy\SensitiveWord\Http\Form;
+
+use Poppy\Framework\Classes\Resp;
+use Poppy\MgrApp\Classes\Widgets\FormWidget;
+use Poppy\SensitiveWord\Action\Word;
+
+class FormSensWordEstablish extends FormWidget
+{
+
+    protected string $title = '添加敏感词';
+
+    public function handle()
+    {
+        $Word = new Word();
+        if (!$Word->establish(input())) {
+            return Resp::error($Word->getError());
+        }
+        return Resp::success('操作成功', 'motion|grid:reload');
+    }
+
+    public function form()
+    {
+        $this->textarea('word', '敏感词')->help('一行一个');
+    }
+}
---
+++ src/Http/Grid/GridSensitiveWord.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Poppy\SensitiveWord\Http\Grid;
+
+use Poppy\MgrApp\Classes\Grid\Column\Render\ActionsRender;
+use Poppy\MgrApp\Classes\Grid\Column\Render\Render;
+use Poppy\MgrApp\Classes\Grid\Tools\Actions;
+use Poppy\MgrApp\Classes\Widgets\FilterWidget;
+use Poppy\MgrApp\Classes\Widgets\TableWidget;
+use Poppy\MgrApp\Http\Grid\GridBase;
+
+class GridSensitiveWord extends GridBase
+{
+
+    public string $title = '敏感词';
+
+    /**
+     */
+    public function table(TableWidget $table)
+    {
+        $table->add('id', "ID")->sortable()->quickId();
+        $table->add('word', "敏感词");
+        $table->action(function (ActionsRender $actions) {
+            /** @var $this Render */
+            $row = $this->getRow();
+            $actions->quickIcon();
+            $actions->request('删除', route_url('py-sensitive-word:api-backend.word.delete', data_get($row, 'id')))
+                ->icon('Close')->danger();
+        })->quickIcon(1);
+    }
+
+
+    public function filter(FilterWidget $filter)
+    {
+        $filter->like('word', '敏感词');
+    }
+
+    public function batch(Actions $actions)
+    {
+        $actions->request('删除', route('py-sensitive-word:api-backend.word.delete'))
+            ->icon('Close')->danger()->confirm();
+    }
+}
